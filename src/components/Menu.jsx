f58afab0ca71fa228495360eaf78981aa49ca8d6
g-akca <gok.akca66@gmail.com>
2026-03-25 12:10:51 +0300

    Add tablet and desktop responsive Menu styles

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,10 +2,10 @@ import closeIcon from "/images/icon-close.svg";
 
 function Menu({ closeMenu }) {
   return (
-    <div className="fixed inset-0 z-50">
+    <div className="fixed inset-0 z-50 desktop:hidden">
       <div className="absolute inset-0 bg-black/50" onClick={closeMenu} />
 
-      <div className="absolute top-0 left-0 z-10 w-screen h-27.5 px-6 bg-white flex items-center justify-between">
+      <div className="absolute top-0 left-0 z-10 w-screen h-27.5 px-6 tablet:px-16 bg-white flex items-center justify-between">
         <button type="button" onClick={closeMenu} className="cursor-pointer">
           <img src={closeIcon} alt="Close menu" />
         </button>
